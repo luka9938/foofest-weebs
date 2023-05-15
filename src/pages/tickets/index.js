e09a68709db17744335b1eb9a91994acd9e6320b
Your Name <luka9938@stud.kea.dk>
2023-05-15 10:51:52 +0200

    cards

--- src/pages/tickets/index.js
+++ src/pages/tickets/index.js
@@ -10,14 +10,35 @@ export default function Tickets() {
         <div className={styles.ticket_container}>
           <article className={styles.article}>
             <h2 className={styles.h2}>Regular</h2>
+            <ul className={styles.ul}>
+              <li>- Alle 8 dage på Foofest uden camping.</li>
+              <li>
+                - Den essentielle pakke til <br />
+                "live hard cum fast"
+              </li>
+            </ul>
+            <h3 className={styles.h1}>799,-</h3>
             <a className={styles.a} href="#">
-              799,-
+              KØB NU
             </a>
           </article>
           <article className={styles.article}>
             <h2 className={styles.h2}>VIP</h2>
+            <ul className={styles.ul}>
+              <li>
+                - Giver adgang til hele festivalpladsen og alle koncerterne samt
+                vores særlige R.I.P.-område.
+              </li>
+              <li>
+                - Eksklusivt udvalg af alt fra lækker mad og cocktails til
+                eftertragtede specialøl
+              </li>
+              <li>- Chillout-område</li>
+              <li>- Hemmelig merchandisebutik med en stærkt begrænset vare.</li>
+            </ul>
+            <h3 className={styles.h1}>1299,-</h3>
             <a className={styles.a} href="#">
-              1299,-
+              KØB NU
             </a>
           </article>
         </div>
