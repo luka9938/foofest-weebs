0dbd6c0a8a1cefbf53b1d5cf652134ff45f77b26
Lukas G <luka9938@stud.kea.dk>
2023-05-15 09:28:22 +0200

    tickets site start

--- src/pages/tickets/index.js
+++ src/pages/tickets/index.js
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <></>;
+import styles from "./tickets.module.css";
+
+export default function Tickets() {
+  return (
+    <>
+      <main>
+        <div className="hero">
+          <h1 className={styles.h1}>TICKETS</h1>
+        </div>
+        <div className={styles.ticket_container}>
+          <article className={styles.article}>
+            <h2 className={styles.h2}>Regular</h2>
+            <a className={styles.a} href="#">
+              799,-
+            </a>
+          </article>
+          <article className={styles.article}>
+            <h2 className={styles.h2}>VIP</h2>
+            <a className={styles.a} href="#">
+              1299,-
+            </a>
+          </article>
+        </div>
+      </main>
+    </>
+  );
 }
