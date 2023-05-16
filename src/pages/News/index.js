45af17086f85b57ba996e325d5b54693dd960f2b
Alex <electromanati@gmail.com>
2023-05-16 14:31:13 +0200

    news knapper ændret

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -3,7 +3,7 @@ import styles from "./news.module.css";
 export default function Home() {
   return (
     <>
-      <h1 className={styles.headline}>Nyheder</h1>
+      <h1 className={styles.headline}>Seneste Nyheder</h1>
 
       <section className={styles.newsContainer}>
         <NewsArticles></NewsArticles>
@@ -17,75 +17,124 @@ function NewsArticles() {
     <>
       <article className={styles.newsArticle}>
         <img className={styles.images} src="/Horns.jpg" alt="news 1" />
-        <p className={styles.paragraph}>
+        <h2 className={styles.h2}>Overskrift</h2>
+        <p className={styles.description}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus non nisl ac tempus. Etiam accumsan massa nec scelerisque pharetra. Nunc dui urna, blandit sit amet mollis eget,
           condimentum a purus. Nullam facilisis neque interdum tristique ultricies.
         </p>
         <p className={styles.dato}>Dato</p>
-        <button className={styles.readMore}>Read more</button>
+        {/* source til buttons: https://uiverse.io/cssbuttons-io/massive-mayfly-74?fbclid=IwAR32KxwShr68PqJUquglPbJkgX2sBOe-unqk1yftTv-R43WAegBI5Zkbyyw */}
+        <button className={styles.learnMore}>
+          <span className={`${styles.circle}`}>
+            <span className={`${styles.icon} ${styles.arrow}`}></span>
+          </span>
+          <span className={styles.buttonText}>Read More</span>
+        </button>
       </article>
       <article className={styles.newsArticle}>
         <img src="" alt="News 2" />
-        <p className={styles.paragraph}>
+        <h2 className={styles.h2}>Overskrift</h2>
+        <p className={styles.description}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus non nisl ac tempus. Etiam accumsan massa nec scelerisque pharetra. Nunc dui urna, blandit sit amet mollis eget,
           condimentum a purus. Nullam facilisis neque interdum tristique ultricies.
         </p>
         <p className={styles.dato}>Dato</p>
-        <button className={styles.readMore}>Read more</button>
+        <button className={styles.learnMore}>
+          <span className={`${styles.circle}`}>
+            <span className={`${styles.icon} ${styles.arrow}`}></span>
+          </span>
+          <span className={styles.buttonText}>Read More</span>
+        </button>
       </article>
       <article className={styles.newsArticle}>
         <img src="" alt="News 3" />
-        <p className={styles.paragraph}>
+        <h2 className={styles.h2}>Overskrift</h2>
+        <p className={styles.description}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus non nisl ac tempus. Etiam accumsan massa nec scelerisque pharetra. Nunc dui urna, blandit sit amet mollis eget,
           condimentum a purus. Nullam facilisis neque interdum tristique ultricies.
         </p>
         <p className={styles.dato}>Dato</p>
-        <button className={styles.readMore}>Read more</button>
+        <button className={styles.learnMore}>
+          <span className={`${styles.circle}`}>
+            <span className={`${styles.icon} ${styles.arrow}`}></span>
+          </span>
+          <span className={styles.buttonText}>Read More</span>
+        </button>
       </article>
       <article className={styles.newsArticle}>
         <img src="" alt="News 4" />
-        <p className={styles.paragraph}>
+        <h2 className={styles.h2}>Overskrift</h2>
+        <p className={styles.description}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus non nisl ac tempus. Etiam accumsan massa nec scelerisque pharetra. Nunc dui urna, blandit sit amet mollis eget,
           condimentum a purus. Nullam facilisis neque interdum tristique ultricies.
         </p>
         <p className={styles.dato}>Dato</p>
-        <button className={styles.readMore}>Read more</button>
+        <button className={styles.learnMore}>
+          <span className={`${styles.circle}`}>
+            <span className={`${styles.icon} ${styles.arrow}`}></span>
+          </span>
+          <span className={styles.buttonText}>Read More</span>
+        </button>
       </article>
       <article className={styles.newsArticle}>
         <img src="" alt="News 5" />
-        <p className={styles.paragraph}>
+        <h2 className={styles.h2}>Overskrift</h2>
+        <p className={styles.description}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus non nisl ac tempus. Etiam accumsan massa nec scelerisque pharetra. Nunc dui urna, blandit sit amet mollis eget,
           condimentum a purus. Nullam facilisis neque interdum tristique ultricies.
         </p>
         <p className={styles.dato}>Dato</p>
-        <button className={styles.readMore}>Read more</button>
+        <button className={styles.learnMore}>
+          <span className={`${styles.circle}`}>
+            <span className={`${styles.icon} ${styles.arrow}`}></span>
+          </span>
+          <span className={styles.buttonText}>Read More</span>
+        </button>
       </article>
       <article className={styles.newsArticle}>
         <img src="" alt="News 6" />
-        <p className={styles.paragraph}>
+        <h2 className={styles.h2}>Overskrift</h2>
+        <p className={styles.description}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus non nisl ac tempus. Etiam accumsan massa nec scelerisque pharetra. Nunc dui urna, blandit sit amet mollis eget,
           condimentum a purus. Nullam facilisis neque interdum tristique ultricies.
         </p>
         <p className={styles.dato}>Dato</p>
-        <button className={styles.readMore}>Read more</button>
+        <button className={styles.learnMore}>
+          <span className={`${styles.circle}`}>
+            <span className={`${styles.icon} ${styles.arrow}`}></span>
+          </span>
+          <span className={styles.buttonText}>Read More</span>
+        </button>
       </article>
       <article className={styles.newsArticle}>
         <img src="" alt="News 7" />
-        <p className={styles.paragraph}>
+        <h2 className={styles.h2}>Overskrift</h2>
+        <p className={styles.description}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus non nisl ac tempus. Etiam accumsan massa nec scelerisque pharetra. Nunc dui urna, blandit sit amet mollis eget,
           condimentum a purus. Nullam facilisis neque interdum tristique ultricies.
         </p>
         <p className={styles.dato}>Dato</p>
-        <button className={styles.readMore}>Read more</button>
+        <button className={styles.learnMore}>
+          <span className={`${styles.circle}`}>
+            <span className={`${styles.icon} ${styles.arrow}`}></span>
+          </span>
+          <span className={styles.buttonText}>Read More</span>
+        </button>
       </article>
       <article className={styles.newsArticle}>
         <img src="" alt="News 8" />
-        <p className={styles.paragraph}>
+        <h2 className={styles.h2}>Overskrift</h2>
+        <p className={styles.description}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus non nisl ac tempus. Etiam accumsan massa nec scelerisque pharetra. Nunc dui urna, blandit sit amet mollis eget,
           condimentum a purus. Nullam facilisis neque interdum tristique ultricies.
         </p>
         <p className={styles.dato}>Dato</p>
-        <button className={styles.readMore}>Read more</button>
+        <button className={styles.learnMore}>
+          <span className={`${styles.circle}`}>
+            <span className={`${styles.icon} ${styles.arrow}`}></span>
+          </span>
+          <span className={styles.buttonText}>Read More</span>
+        </button>
       </article>
     </>
   );
