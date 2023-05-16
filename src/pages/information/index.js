2da5e2c4650b09a61727ff2cd63e6913d7214839
Vijasan Vasantharajan <vijasan.v@hotmail.com>
2023-05-16 15:16:27 +0200

    Google maps added

--- src/pages/information/index.js
+++ src/pages/information/index.js
@@ -1,14 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import styles from "./practical.module.css";
+import { Map, Marker } from "google-maps-react";
 
 const PracticalPage = () => {
   const [isLocationOpen, setLocationOpen] = useState(false);
   const [isDateOpen, setDateOpen] = useState(false);
   const [isTimeOpen, setTimeOpen] = useState(false);
+  const [mapCenter, setMapCenter] = useState({ lat: 0, lng: 0 });
+  const [googleMapsLoaded, setGoogleMapsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!googleMapsLoaded) {
+      const script = document.createElement("script");
+      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDi6Fdp3SAkvyIPJyDxJmPDr3anwQkulcY`;
+      script.async = true;
+      script.onload = () => {
+        setGoogleMapsLoaded(true);
+      };
+      document.head.appendChild(script);
+    }
+  }, []);
 
   const handleLocationClick = () => {
     setLocationOpen(!isLocationOpen);
+
+    if (!isLocationOpen) {
+      setMapCenter({ lat: 55.76109378592378, lng: 12.579019359672232 });
+    }
   };
 
   const handleDateClick = () => {
@@ -41,9 +60,29 @@ const PracticalPage = () => {
               onClick={handleLocationClick}
             >
               Location: Your Festival Location
-              {isLocationOpen && (
+              {isLocationOpen ? (
+                <button>Hide Map</button>
+              ) : (
+                <button>Show Map</button>
+              )}
+              {isLocationOpen && googleMapsLoaded && (
                 <div className={styles.additionalInfo}>
                   Additional information about the location.
+                  <div className={styles.mapContainer}>
+                    <Map
+                      google={window.google}
+                      zoom={14}
+                      initialCenter={mapCenter}
+                      className={styles.map}
+                    >
+                      <Marker
+                        position={{
+                          lat: 55.76074970251176,
+                          lng: 12.578783325298094,
+                        }}
+                      />
+                    </Map>
+                  </div>
                 </div>
               )}
             </li>
