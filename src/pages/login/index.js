54ecd1664c4c528320bae05745681206592254d1
steffen kroyer <stef5450@stud.kea.dk>
2023-05-17 14:37:11 +0200

    made a fake login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,26 +1,27 @@
 import React from "react";
+import styles from "./login.module.css";
 
 export default function Login() {
   return (
-    <div className="container">
-      <div className="card">
-        <a className="signup">Sign Up</a>
-        <div className="inputBox1">
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <a className={styles.signup}>Sign Up</a>
+        <div className={styles.inputBox1}>
           <input type="text" required="required" />
-          <span className="user">Email</span>
+          <span className={styles.user}>Email</span>
         </div>
 
-        <div className="inputBox">
+        <div className={styles.inputBox}>
           <input type="text" required="required" />
           <span>Username</span>
         </div>
 
-        <div className="inputBox">
+        <div className={styles.inputBox}>
           <input type="password" required="required" />
           <span>Password</span>
         </div>
 
-        <button className="enter">Enter</button>
+        <button className={styles.enter}>Enter</button>
       </div>
     </div>
   );
