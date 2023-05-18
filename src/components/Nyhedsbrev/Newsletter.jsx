a8c423afe3e8b1317857bfcc9ba9577600b4bada
Vijasan Vasantharajan <vijasan.v@hotmail.com>
2023-05-18 12:29:43 +0200

    Confirmation page added

--- src/components/Nyhedsbrev/Newsletter.jsx
+++ src/components/Nyhedsbrev/Newsletter.jsx
@@ -1,19 +1,33 @@
 import styles from "./Newsletter.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function Newsletter() {
   const [email, setEmail] = useState("");
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [previousPage, setPreviousPage] = useState(null);
 
-  const handleSubmit = (element) => {
-    element.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
     // Perform actions with the submitted email, such as sending it to a server
     console.log("Submitted email:", email);
     // Reset the form
     setEmail("");
+
+    // Show the confirmation popup
+    setShowConfirmation(true);
+
+    // Save the previous page to return to after 3 seconds
+    setPreviousPage(window.location.pathname);
+
+    // Hide the confirmation popup after 3 seconds and return to the previous page
+    setTimeout(() => {
+      setShowConfirmation(false);
+      setPreviousPage(null);
+    }, 3000);
   };
 
-  const handleChange = (element) => {
-    setEmail(element.target.value);
+  const handleChange = (e) => {
+    setEmail(e.target.value);
   };
 
   return (
@@ -22,16 +36,37 @@ export function Newsletter() {
         <div className={styles.border}>
           <h2 className={styles.h2}>Subscribe to our Newsletter</h2>
           <form className={styles.form} onSubmit={handleSubmit}>
-            <label className={styles.label} for="news-email">
+            <label className={styles.label} htmlFor="news-email">
               Email
             </label>
-            <input id="news-email" className={styles.input} type="email" value={email} onChange={handleChange} placeholder="Enter your email" required />
+            <input
+              id="news-email"
+              className={styles.input}
+              type="email"
+              value={email}
+              onChange={handleChange}
+              placeholder="Enter your email"
+              required
+            />
             <button className={styles.button} type="submit">
               Subscribe
             </button>
           </form>
         </div>
       </div>
+      {showConfirmation && (
+        <div className={styles.confirmationPopup}>
+          <div className={styles.popupContent}>
+            <p>Thank you for signing up!</p>
+            <p>You will receive our newsletter shortly.</p>
+          </div>
+        </div>
+      )}
+      {previousPage && (
+        <div className={styles.returnMessage}>
+          <p>Returning to the previous page...</p>
+        </div>
+      )}
     </>
   );
 }
---
+++ src/pages/confirmation.js
@@ -0,0 +1,11 @@
+const ConfirmationPage = () => {
+  return (
+    <div>
+      <h1>Thank you for signing up!</h1>
+      <p>You will receive our newsletter shortly.</p>
+      <p>Redirecting to the homepage...</p>
+    </div>
+  );
+};
+
+export default ConfirmationPage;
