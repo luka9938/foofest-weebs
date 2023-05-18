b0dfc922297e00bb00f4adbd7720a779ffa76a36
Alex <electromanati@gmail.com>
2023-05-18 11:24:20 +0200

    Sponsors grid done

--- src/components/Nyhedsbrev/Newsletter.jsx
+++ src/components/Nyhedsbrev/Newsletter.jsx
@@ -4,16 +4,16 @@ import { useState } from "react";
 export function Newsletter() {
   const [email, setEmail] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (element) => {
+    element.preventDefault();
     // Perform actions with the submitted email, such as sending it to a server
     console.log("Submitted email:", email);
     // Reset the form
     setEmail("");
   };
 
-  const handleChange = (e) => {
-    setEmail(e.target.value);
+  const handleChange = (element) => {
+    setEmail(element.target.value);
   };
 
   return (
@@ -22,8 +22,10 @@ export function Newsletter() {
         <div className={styles.border}>
           <h2 className={styles.h2}>Subscribe to our Newsletter</h2>
           <form className={styles.form} onSubmit={handleSubmit}>
-            <label className={styles.label}>Email</label>
-            <input className={styles.input} type="email" value={email} onChange={handleChange} placeholder="Enter your email" required />
+            <label className={styles.label} for="news-email">
+              Email
+            </label>
+            <input id="news-email" className={styles.input} type="email" value={email} onChange={handleChange} placeholder="Enter your email" required />
             <button className={styles.button} type="submit">
               Subscribe
             </button>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { Header } from "@/components/Header/Header";
 import { Footer } from "@/components/Footer/Footer";
 import { Newsletter } from "@/components/Nyhedsbrev/Newsletter.jsx";
+import { Sponsors } from "@/components/Sponsors/Sponsors.jsx";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
       <Header />
       <Component {...pageProps} />
       <Newsletter></Newsletter>
+      <Sponsors></Sponsors>
       <Footer />
     </>
   );
