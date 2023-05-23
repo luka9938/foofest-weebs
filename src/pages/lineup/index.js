651742bbec7bb41f7cf9830d2e130605a2689a5b
Vijasan Vasantharajan <vijasan.v@hotmail.com>
2023-05-23 15:14:12 +0200

    APIII

--- src/pages/lineup/index.js
+++ src/pages/lineup/index.js
@@ -1,138 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./lineup.module.css";
 
 export default function Lineup() {
   const [showPopup, setShowPopup] = useState(false);
+  const [bands, setBands] = useState([]);
+  const [schedule, setSchedule] = useState([]);
 
-  const togglePopup = () => {
+  useEffect(() => {
+    // Fetch bands data from the API
+    fetch("https://sunrise-innovative-pediatrician.glitch.me/bands")
+      .then((response) => response.json())
+      .then((data) => setBands(data))
+      .catch((error) => console.error(error));
+
+    // Fetch schedule data from the API
+    fetch("https://sunrise-innovative-pediatrician.glitch.me/schedule")
+      .then((response) => response.json())
+      .then((data) => setSchedule(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const togglePopup = (band) => {
     setShowPopup(!showPopup);
+    setShowPopup(band);
   };
 
   const closePopup = () => {
     setShowPopup(false);
   };
 
+  const getBandSchedule = (bandName) => {
+    for (const stage of Object.keys(schedule)) {
+      const daySchedule = schedule[stage];
+      for (const day of Object.keys(daySchedule)) {
+        const slots = daySchedule[day];
+        for (const slot of slots) {
+          if (slot.act === bandName) {
+            return {
+              stageId: stage,
+              start: slot.start,
+              end: slot.end,
+              day: day,
+            };
+          }
+        }
+      }
+    }
+    return {};
+  };
+
+  const getStageName = (stageId) => {
+    if (stageId === "Midgard") {
+      return "Midgard";
+    } else if (stageId === "Vanaheim") {
+      return "Vanaheim";
+    } else if (stageId === "Jotunheim") {
+      return "Jotunheim";
+    }
+    return "Unknown";
+  };
+
   return (
     <div className={styles.container}>
-      <div className={`${styles.card} ${styles.bigCard} `}>
-        <div className={styles.cardimg}></div>
-        <div className={styles.cardinfo}>
-          <p className={styles.title} onClick={togglePopup}>
-            Name
-          </p>
-          <p className={styles.subtitle}>Dato</p>
+      {bands.map((band) => (
+        <div key={band.id} className={`${styles.card} ${styles.smallCard}`}>
+          <div className={styles.cardimg}></div>
+          <div className={styles.cardinfo}>
+            <p className={styles.title} onClick={() => togglePopup(band)}>
+              {band.name}
+            </p>
+            <p className={styles.subtitle}>{band.dato}</p>
+          </div>
         </div>
-      </div>
-      <div className={`${styles.card} ${styles.bigCard}`}>
-        <div className={styles.cardimg}></div>
-        <div className={styles.cardinfo}>
-          <p className={styles.title} onClick={togglePopup}>
-            Name
-          </p>
-          <p className={styles.subtitle}>Dato</p>
-        </div>
-      </div>
-      <div className={`${styles.card} ${styles.smallCard}`}>
-        <div className={styles.cardimg}></div>
-        <div className={styles.cardinfo}>
-          <p className={styles.title} onClick={togglePopup}>
-            Name
-          </p>
-          <p className={styles.subtitle}>Dato</p>
-        </div>
-      </div>
-      <div className={`${styles.card} ${styles.smallCard}`}>
-        <div className={styles.cardimg}></div>
-        <div className={styles.cardinfo}>
-          <p className={styles.title} onClick={togglePopup}>
-            Name
-          </p>
-          <p className={styles.subtitle}>Dato</p>
-        </div>
-      </div>
-      <div className={`${styles.card} ${styles.smallCard}`}>
-        <div className={styles.cardimg}></div>
-        <div className={styles.cardinfo}>
-          <p className={styles.title} onClick={togglePopup}>
-            Name
-          </p>
-          <p className={styles.subtitle}>Dato</p>
-        </div>
-      </div>
-      <div className={`${styles.card} ${styles.smallerCard}`}>
-        <div className={styles.cardimg}></div>
-        <div className={styles.cardinfo}>
-          <p className={styles.title} onClick={togglePopup}>
-            Name
-          </p>
-          <p className={styles.subtitle}>Dato</p>
-        </div>
-      </div>
-      <div className={`${styles.card} ${styles.smallerCard}`}>
-        <div className={styles.cardimg}></div>
-        <div className={styles.cardinfo}>
-          <p className={styles.title} onClick={togglePopup}>
-            Name
-          </p>
-          <p className={styles.subtitle}>Dato</p>
-        </div>
-      </div>
-      <div className={`${styles.card} ${styles.smallerCard}`}>
-        <div className={styles.cardimg}></div>
-        <div className={styles.cardinfo}>
-          <p className={styles.title} onClick={togglePopup}>
-            Name
-          </p>
-          <p className={styles.subtitle}>Dato</p>
-        </div>
-      </div>
-      <div className={`${styles.card} ${styles.smallerCard}`}>
-        <div className={styles.cardimg}></div>
-        <div className={styles.cardinfo}>
-          <p className={styles.title} onClick={togglePopup}>
-            Name
-          </p>
-          <p className={styles.subtitle}>Dato</p>
-        </div>
-      </div>
-      <div className={`${styles.card} ${styles.smallerCard}`}>
-        <div className={styles.cardimg}></div>
-        <div className={styles.cardinfo}>
-          <p className={styles.title} onClick={togglePopup}>
-            Name
-          </p>
-          <p className={styles.subtitle}>Dato</p>
-        </div>
-      </div>
-      <div className={`${styles.card} ${styles.smallerCard}`}>
-        <div className={styles.cardimg}></div>
-        <div className={styles.cardinfo}>
-          <p className={styles.title} onClick={togglePopup}>
-            Name
-          </p>
-          <p className={styles.subtitle}>Dato</p>
-        </div>
-      </div>
-      <div className={`${styles.card} ${styles.smallerCard}`}>
-        <div className={styles.cardimg}></div>
-        <div className={styles.cardinfo}>
-          <p className={styles.title} onClick={togglePopup}>
-            Name
-          </p>
-          <p className={styles.subtitle}>Dato</p>
-        </div>
-      </div>
-      <div className={`${styles.card} ${styles.smallerCard}`}>
-        <div className={styles.cardimg}></div>
-        <div className={styles.cardinfo}>
-          <p className={styles.title} onClick={togglePopup}>
-            Name
-          </p>
-          <p className={styles.subtitle}>Dato</p>
-        </div>
-      </div>
-
-      {/* ...other cards... */}
+      ))}
       {showPopup && (
         <div className={styles.popup}>
           <button className={styles.closeButton} onClick={closePopup}>
@@ -143,22 +83,23 @@ export default function Lineup() {
               <iframe
                 className={styles.spotify}
                 src="https://open.spotify.com/embed/playlist/5SGaFih3XIAg9emzcqeoWC?utm_source=generator&theme=0"
-                allowfullscreen=""
+                allowFullScreen=""
                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                 loading="lazy"
               ></iframe>
             </div>
-            <h2 className={styles.popuptitle}>Popup Title</h2>
+            <h2 className={styles.popuptitle}>{showPopup.name}</h2>
+            <h3 className={styles.popupsubtitle}>Scheduled Stage:</h3>
             <p className={styles.popupdescription}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget metus quis eros
-              elementum euismod sed eget dolor. Maecenas vitae cursus neque. Suspendisse rutrum id
-              est a sollicitudin. Cras non rhoncus lectus, eu ultrices ipsum. Morbi ac dolor quis
-              diam luctus hendrerit. Fusce at rutrum eros, quis viverra enim. In bibendum egestas
-              leo, vitae aliquet mi ultricies et. Fusce ultricies libero augue, sit amet posuere
-              turpis lobortis vitae. Sed quis consectetur lacus, a ornare erat. Pellentesque
-              egestas, erat eget cursus elementum, erat enim lacinia mi, vitae tristique sapien
-              sapien lacinia turpis. Duis semper, ipsum id convallis sagittis, dui mi porttitor
-              odio, in mollis dui erat quis lorem.
+              {getStageName(getBandSchedule(showPopup.name)?.stageId)}
+            </p>
+            <h3 className={styles.popupsubtitle}>Start Time:</h3>
+            <p className={styles.popupdescription}>
+              {getBandSchedule(showPopup.name)?.start}
+            </p>
+            <h3 className={styles.popupsubtitle}>End Time:</h3>
+            <p className={styles.popupdescription}>
+              {getBandSchedule(showPopup.name)?.end}
             </p>
           </div>
         </div>
