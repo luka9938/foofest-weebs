b5c3f7e34fb943828b52537a8815daf172c4ef5b
steffen kroyer <stef5450@stud.kea.dk>
2023-05-23 13:28:28 +0200

    added a page for when you login

---
+++ src/pages/frivillig/index.js
@@ -0,0 +1,74 @@
+import styles from "./frivillig.module.css";
+
+export default function Frivillig() {
+  return (
+    <>
+      <h2 className={styles.subtitle}>Welcome to FooFest 2023!!!!!</h2>
+      <header className={styles.header}>
+        <div className={styles.headertext}>
+          <h1>Kære frivillig</h1>
+          <p>
+            Non utriuque solatium amet netus nobis cum metus securus fecerunt oppressus eu
+            oportunitatis error est quae interpretes corporis - v tot ullo s Verius. Non utriuque
+            solatium amet netus nobis cum metus securus fecerunt oppressus eu oportunitatis error
+            est quae interpretes corporis - v tot ullo s Verius. Non utriuque solatium amet netus
+            nobis cum metus securus fecerunt oppressus eu oportunitatis error est quae interpretes
+            corporis - v tot ullo s Verius. Non utriuque solatium amet netus nobis cum metus securus
+            fecerunt oppressus eu oportunitatis error est quae interpretes corporis - v tot ullo s
+            Verius.
+          </p>
+        </div>
+        <div className={styles.headergraphics}></div>
+      </header>
+
+      <div id="frivillig">
+        <div className={styles.container}>
+          <div className={styles.cardslist}>
+            <div>
+              <h3>My Tecket</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie lacus id
+                magna viverra laoreet. Aliquam id nunc ornare, tempus arcu ac, efficitur ligula.
+              </p>
+            </div>
+            <div>
+              <h3>My Teams</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie lacus id
+                magna viverra laoreet. Aliquam id nunc ornare, tempus arcu ac, efficitur ligula.
+              </p>
+            </div>
+            <div>
+              <h3>My Shift</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie lacus id
+                magna viverra laoreet. Aliquam id nunc ornare, tempus arcu ac, efficitur ligula.
+              </p>
+            </div>
+            <div>
+              <h3>Shift Excuse</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie lacus id
+                magna viverra laoreet. Aliquam id nunc ornare, tempus arcu ac, efficitur ligula.
+              </p>
+            </div>
+            <div>
+              <h3>News</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie lacus id
+                magna viverra laoreet. Aliquam id nunc ornare, tempus arcu ac, efficitur ligula.
+              </p>
+            </div>
+            <div>
+              <h3>FQA</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie lacus id
+                magna viverra laoreet. Aliquam id nunc ornare, tempus arcu ac, efficitur ligula.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,27 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./login.module.css";
 
 export default function Login() {
+  const [loginMode, setLoginMode] = useState(true);
+  const [signupMessage, setSignupMessage] = useState("");
+
+  const handleToggleMode = () => {
+    setLoginMode(!loginMode);
+    setSignupMessage("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formDataObject = Object.fromEntries(formData.entries());
+    console.log(formDataObject); // You can perform further actions with the form data
+    if (!loginMode) {
+      setSignupMessage("Signup successful!");
+    } else {
+      // Perform login actions here
+      // Redirect to a new site after successful login
+      window.location.href = "./frivillig";
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <a className={styles.signup}>Sign Up</a>
-        <div className={styles.inputBox1}>
-          <input type="text" required="required" />
-          <span className={styles.user}>Email</span>
-        </div>
+        <a className={styles.signup}>{loginMode ? "Login" : "Sign up"}</a>
+        {!loginMode && signupMessage && <p className={styles.signupMessage}>{signupMessage}</p>}
+        <form onSubmit={handleSubmit}>
+          {!loginMode && (
+            <div className={styles.inputBox1}>
+              <input type="text" name="email" required />
+              <span className={styles.user}>Email</span>
+            </div>
+          )}
 
-        <div className={styles.inputBox}>
-          <input type="text" required="required" />
-          <span>Username</span>
-        </div>
+          <div className={styles.inputBox}>
+            <input type="text" name="username" required />
+            <span>Username</span>
+          </div>
 
-        <div className={styles.inputBox}>
-          <input type="password" required="required" />
-          <span>Password</span>
-        </div>
+          <div className={styles.inputBox}>
+            <input type="password" name="password" required />
+            <span>Password</span>
+          </div>
 
-        <button className={styles.enter}>Enter</button>
+          <button type="submit" className={styles.enter}>
+            {loginMode ? "Login" : "Sign Up"}
+          </button>
+        </form>
+        <a className={styles.toggle} onClick={handleToggleMode}>
+          {loginMode ? "Sign Up" : "Login"}
+        </a>
       </div>
     </div>
   );
